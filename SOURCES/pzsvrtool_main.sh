0bbb2a23a8e51c308e5735d39991c4bd3630ec02
Lu5ck <6626980+Lu5ck@users.noreply.github.com>
2025-01-10 03:35:06 +0800

    Restore only VmArgs In prepration for unstable B42, replacing the entire json is stupid as classpath likely to have changes. Thus, we restore only the VmArgs

--- SOURCES/pzsvrtool_main.sh
+++ SOURCES/pzsvrtool_main.sh
@@ -116,13 +116,23 @@ install_steam() {
     # Restore json if flag
     if [[ -n ${installJson} ]]; then
         if [[ -f ~/${zomboidCoreServerFolderName}/ProjectZomboid64.json && -f ~/${zomboidCoreServerFolderName}/ProjectZomboid64.json.old ]]; then
+            export JQ_SPACES=1
+            local backupVmArgs=$(jq ".vmArgs" "~/${zomboidCoreServerFolderName}/ProjectZomboid64.json.old")
+            local originalPerm=$(stat -c "%a" "~/${zomboidCoreServerFolderName}/ProjectZomboid64.json")
+            jq --argjson backupVmArgs "${backupVmArgs}" ".vmArgs = ${backupVmArgs}" "~/${zomboidCoreServerFolderName}/ProjectZomboid64.json" | jq "." | sed "s/  /\t/g" > "~/${zomboidCoreServerFolderName}/ProjectZomboid64.json.temp"
             mv -f ~/${zomboidCoreServerFolderName}/ProjectZomboid64.json ~/${zomboidCoreServerFolderName}/ProjectZomboid64.json.original
-            mv -f ~/${zomboidCoreServerFolderName}/ProjectZomboid64.json.old ~/${zomboidCoreServerFolderName}/ProjectZomboid64.json
+            mv -f ~/${zomboidCoreServerFolderName}/ProjectZomboid64.json.temp ~/${zomboidCoreServerFolderName}/ProjectZomboid64.json
+            chmod "${originalPerm}" ~/${zomboidCoreServerFolderName}/ProjectZomboid64.json
         fi
 
         if [[ -f ~/${zomboidCoreServerFolderName}/ProjectZomboid32.json && -f ~/${zomboidCoreServerFolderName}/ProjectZomboid32.json.old ]]; then
+            export JQ_SPACES=1
+            local backupVmArgs=$(jq ".vmArgs" "~/${zomboidCoreServerFolderName}/ProjectZomboid32.json.old")
+            local originalPerm=$(stat -c "%a" "~/${zomboidCoreServerFolderName}/ProjectZomboid32.json")
+            jq --argjson backupVmArgs "${backupVmArgs}" ".vmArgs = ${backupVmArgs}" "~/${zomboidCoreServerFolderName}/ProjectZomboid32.json" | jq "." | sed "s/  /\t/g" > "~/${zomboidCoreServerFolderName}/ProjectZomboid32.json.temp"
             mv -f ~/${zomboidCoreServerFolderName}/ProjectZomboid32.json ~/${zomboidCoreServerFolderName}/ProjectZomboid32.json.original
-            mv -f ~/${zomboidCoreServerFolderName}/ProjectZomboid32.json.old ~/${zomboidCoreServerFolderName}/ProjectZomboid32.json
+            mv -f ~/${zomboidCoreServerFolderName}/ProjectZomboid32.json.temp ~/${zomboidCoreServerFolderName}/ProjectZomboid32.json
+            chmod "${originalPerm}" ~/${zomboidCoreServerFolderName}/ProjectZomboid32.json
         fi
     fi
 
